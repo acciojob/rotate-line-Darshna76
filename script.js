3e11707584e1de3eaabe525975aea4b9e7d75b25
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-16 23:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var line = document.getElementById("line");
+		var angle = 0;
+
+		function rotateLine() {
+			angle += 5;
+			line.style.transform = "rotate(" + angle + "deg)";
+			window.requestAnimationFrame(rotateLine);
+		}
+
+		window.requestAnimationFrame(rotateLine);
